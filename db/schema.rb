e3fe8d7b214a1f4afd50b30319d78a693a036acd
mthorry <mthorry@gmail.com>
2017-08-23 18:01:53 -0400

    added friends events, styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823170836) do
+ActiveRecord::Schema.define(version: 20170823210613) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170823170836) do
     t.integer "location_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
   end
 
 end
